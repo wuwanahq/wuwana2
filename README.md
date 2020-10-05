67a33f801bbe98512446ed8dadf3d00d42d36261
Vince <vincent.boursier@gmail.com>
2020-10-05 13:18:19 -0400

    work in progress...

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Wuwana v2
 
-Business directory project used for https://www.wuwana.com
+Simple business directory project used for https://www.wuwana.com
 
 ## How to test locally
 
-Open a Terminal and clone the project source with Git.  
+Open a Terminal and clone the project sources with Git.  
 Then, in the project directory, run the PHP built-in web server:
 
 ```
 git clone https://github.com/wuwanahq/wuwana2.git
 cd wuwana2
-php -S localhost:8000
+php -S 0.0.0.0:8000
 ```
 
 Now you can open the website with your browser:
@@ -19,6 +19,16 @@ Now you can open the website with your browser:
 - Go to http://localhost:8000 To watch your data on the homepage
 
 To update your local version: `git pull`
+
+### How to test on a mobile device
+
+Find the local IP address of your computer that run PHP.
+
+- On **Mac** or **Linux** open a new Terminal and type: `ifconfig | grep 'inet \(192\|10\)\.'`
+- On **Windows** open a new Command Prompt and type: `ipconfig`
+
+Usually a local IP ends with a number smaller than 100 and starts by 10 or 192.168...  
+Now that you know your IP address, you can open the browser on your mobile device and navigate to the same URL as before by replacing “localhost” with your local IP address.
 
 ## How to deploy on a shared web hosting
 
--- homepage/view.php
+++ homepage/view.php
@@ -9,18 +9,21 @@
 <body>
 	<div id="popup">
 		<div>
-			<a href="#" class="button" style="float:right">×</a>
-			<img src="static/logo-circle.png" width="96" height="96"><br><br>
+			<a href="#" class="ButtonRound">×</a>
+			<img src="static/logo-circle.png"><br><br>
 			<p>Encontrar un buen proveedor de café de especialidad debería ser más fácil.<br>¿Por qué no lo es?</p>
 			<p>Los micro-tostadores no tienen los recursos necesarios para competir con las grandes empresas. Mientras que estas utilizan su dinero para contratar agencias y “expertos del marketing” para aparecer en las primeras páginas de Google, los comercios independientes utilizan todos sus recursos para ofrecer el mejor producto posible. Además, es un esfuerzo enorme convertise en un vendedor de Amazon Business. Aparte de cobrar unas comisiones desorbitadas, Amazon hace el proceso de registro como vendedor una pesadilla.</p>
 			<p>Estas barreras dejan a la mayoría de tostadores independientes fuera de la revolución digital. En Wuwana queremos cambiarlo.</p>
 			<p>Estamos creando un directorio moderno y digital que de apoyo a las pequeñas cafeterías y a los tostadores locales. Una página honesta y transparente donde serás capaz de filtrar los comercios locales por categoría, visitar sus páginas web para conocerles mejor y contactarles directamente. Trabajamos con un grupo de cafeterías y micro-tostadores de Madrid en la creación de este proyecto.</p>
 			<p>Apoya a los comercios independientes del café. Síguenos en Twitter para ser de los primeros en probar nuestra beta a finales de octubre 2020.</p>
-			<a href="https://twitter.com/wuwanahq" target="_blank">Síguenos en Twitter</a>
+			<a class="ButtonSquare" href="https://twitter.com/wuwanahq" target="_blank">Síguenos en Twitter</a>
 		</div>
 	</div>
-	<form class="menu" method="get">
-		<div class="logo"><a href="/"><img src="static/logo.png"></a></div>
+	<div class="MenuMobile1">
+		<a href="/"><img class="Logo" src="static/wuwana-black.png"></a>
+		<span class="Symbol">≡</span>
+	</div>
+	<form class="MenuMobile2" method="get">
 		<dl>
 			<dt>Categorías</dt>
 			<dt>
@@ -60,34 +63,75 @@
 			?>
 		</dl>
 	</form>
-	<div class="content">
-		<div class="hero" style="position:relative">
+	<form class="Menu" method="get">
+		<div class="Logo"><a href="/"><img src="static/wuwana-white.png"></a></div>
+		<dl>
+			<dt>Categorías</dt>
+			<dt>
+				<input id="c" type="checkbox" name="cat" onchange="this.form.submit()"
+					<?php if ($selectedCategories == []) { echo 'checked disabled'; } ?>>
+				<label for="c">Todas las categorías</label>
+			</dt>
+			<?php
+				foreach ($categories as $id => $category)
+				{
+					echo '<dd><input type="checkbox" name="cat', $id, '" id="C', $id, '"';
+
+					if (in_array($id, $selectedCategories))
+					{ echo ' checked'; }
+
+					echo ' onchange="this.form.submit()"><label for="C', $id, '">', $category->spanish, '</label></dd>';
+				}
+			?>
+		</dl>
+		<dl>
+			<dt>Comunidades autónomas</dt>
+			<dt>
+				<input id="r" type="checkbox" name="region" onchange="this.form.submit()"
+					<?php if ($selectedRegions == []) { echo 'checked disabled'; } ?>>
+				<label for="r">Todas las comunidades</label>
+			</dt>
+			<?php
+				foreach ($locations as $id => $location)
+				{
+					echo '<dd><input type="checkbox" name="region', $id, '" id="L', $id, '"';
+
+					if (in_array($id, $selectedRegions))
+					{ echo ' checked'; }
+
+					echo ' onchange="this.form.submit()"><label for="L', $id, '">', $location->region, '</label></dd>';
+				}
+			?>
+		</dl>
+	</form>
+	<div class="Content">
+		<div class="Hero" style="position:relative">
 			<br><span style="font-size:3rem; font-weight:900">¿Estás buscando café de especialidad?</span><br>
 			<br><span style="font-size:1.5em">Encuentra los proveedores que necesitas rápidamente.</span>
-			<br><a href="#popup" class="button">
+			<br><a href="#popup" class="ButtonRound">
 				¿Qué es Wuwana?
 			</a>
 		</div>
-		<span class="title">Las empresas</span>
+		<span class="Title">Las empresas</span>
 		<div style="text-align:center; margin-top:24px">
 			<?php
 				foreach ($companies as $company)
 				{
-					echo '<div class="card">';
+					echo '<div class="Card">';
 
 					if ($company->region > 0)
 					{
-						echo '<span class="geoloc"><img src="static/geoloc.png">',
+						echo '<span class="GeoLoc"><img src="static/geoloc.png">',
 							$locations[$company->region]->region,
 						'</span>';
 					}
 
 					echo '<img src="', $company->logo, '">',
-						'<p class="text"><span class="title">', $company->name, '</span>',
+						'<p class="Text"><span class="Title">', $company->name, '</span>',
 						'<br><br>', $company->description, '</p><hr>';
 
 					foreach ($company->categories as $category)
-					{ echo '<span class="button">', $categories[$category]->spanish, '</span>'; }
+					{ echo '<span class="Tag">', $categories[$category]->spanish, '</span>'; }
 
 					echo '<br><br>';
 
