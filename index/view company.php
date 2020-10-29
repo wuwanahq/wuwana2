a4fc3f232f7d82d5eb6050948c2cb664962882dd
Vince <vincent.boursier@gmail.com>
2020-10-29 14:55:19 -0400

    new views for homepage and company page

---
+++ index/view company.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="<?php echo TEXT[5], ' ', TEXT[6] ?>">
+	<title><?php echo TEXT[0] ?> | Wuwana</title>
+	<link rel="icon" type="image/png" href="/static/favicon/16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/static/favicon/32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/static/favicon/48.png" sizes="48x48">
+	<link rel="icon" type="image/png" href="/static/favicon/64.png" sizes="64x64">
+	<link rel="icon" type="image/png" href="/static/favicon/96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/static/favicon/160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="/static/favicon/196.png" sizes="196x196">
+	<link rel="apple-touch-icon" href="/static/favicon/57.png" sizes="57x57">
+	<link rel="apple-touch-icon" href="/static/favicon/60.png" sizes="60x60">
+	<link rel="apple-touch-icon" href="/static/favicon/72.png" sizes="72x72">
+	<link rel="apple-touch-icon" href="/static/favicon/76.png" sizes="76x76">
+	<link rel="apple-touch-icon" href="/static/favicon/114.png" sizes="114x114">
+	<link rel="apple-touch-icon" href="/static/favicon/120.png" sizes="120x120">
+	<link rel="apple-touch-icon" href="/static/favicon/144.png" sizes="144x144">
+	<link rel="apple-touch-icon" href="/static/favicon/152.png" sizes="152x152">
+	<link rel="apple-touch-icon" href="/static/favicon/180.png" sizes="180x180">
+	<link rel="stylesheet" type="text/css" href="/static/style.css">
+	<script src="/static/es5.js" defer></script>
+</head>
+<body>
+	<div class="Button ToTop" id="ToTop">
+		<a href="#"><img src="/static/icon/arrow-circle-top.svg">Volver arriba</a>
+	</div>
+	<header class="HeaderBar">
+		<div class="HeaderContainer">
+			<div class="HeaderLogo"><img src="/static/wuwana-black.svg"></div>
+		</div>
+	</header>
+	<div class="Container">
+		<div class="ColumnLeft Company">
+			<div class="Box Profile"><!-- Company About -->
+				<div class="Logo Profile">
+					<img src="/static/background/117926628_3215393278552514_8264428497985741185_n1.png">
+				</div>
+				<h2 class="ProfileName">Camden Coffee Roaster</h2>
+				<div class="Category">
+					<span class="Label">Tostador</span>
+					<span class="Label">Cafeteria</span>
+				</div>
+				<div class="Tag Region">Cataluna</div>
+				<hr class="ProfileLine">
+				<h3 class="ProfileHeading">Descripcion en Instagram</h3>
+				<br><br>
+				<hr class="ProfileLine">
+				<h3 class="ProfileHeading">Por que Camden Coffee Roaster?</h3>
+				<div class="BadgeArea Profile">
+					<div class="ItemLabelBadge">
+						<img src="/static/badge/google-review.svg">
+						<span class="Label Circle">Google review</span>
+					</div>
+					<div class="ItemLabelBadge">
+						<img src="/static/badge/sustainability.svg">
+						<span class="Label Circle">Sostenible</span>
+					</div>
+					<div class="ItemLabelBadge">
+						<img src="/static/badge/social-impact.svg">
+						<span class="Label Circle">Compromiso social</span>
+					</div>
+				</div>
+				<hr class="ProfileLine">
+				<h3 class="ProfileHeading">Contacta con Camden Coffee Roaster</h3>
+				<div class="AboutSocial">
+					<div class="ItemLabel">
+						<div class="Button Circle"><img src="/static/icon/instagram.svg"></div>
+						<span class="Label Circle">Instagram</span>
+					</div>
+					<div class="ItemLabel">
+						<div class="Button Circle"><img src="/static/icon/whatsapp.svg"></div>
+						<span class="Label Circle">Whatsapp</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ColumnMain">
+			<h1>Fotos de Camden Coffee Roaster en Instagram</h1>
+			<div class="Box Aspect">
+				<div class="Button Absolute"><img src="/static/icon/instagram.svg"> Ver en Instagram</div>
+
+				<div class="ProfileInstagram">
+					<div class="InstagramRow">
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+					</div>
+					<div class="InstagramRow">
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png" >
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="Box Aspect Mobile">
+				<div class="Button Absolute"><img src="/static/icon/instagram.svg"> Ver en Instagram</div>
+				<div class="ProfileInstagram">
+					<div class="InstagramRow">
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+					</div>
+					<div class="InstagramRow">
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+					</div>
+					<div class="InstagramRow">
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png" >
+						</div>
+						<div class="InstagramPicture">
+							<img src="/static/background/ali-yahya-7_AZi5Fe-rU-unsplash.png">
+						</div>
+					</div>
+				</div>
+			</div>
+			<h1>Mapa</h1>
+			<div class="Box Test"></div>
+			<div class="Button Center"><img src="/static/icon/home.svg"> Volver a la pagina principal</div>
+		</div>
+	</div>
+</body>
+</html>
