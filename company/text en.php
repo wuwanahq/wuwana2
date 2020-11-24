02cfa45db10438fd3ab99b2ff47dd236d0a24374
levogirar <yonathanchu@gmail.com>
2020-11-24 11:07:07 -0500

    Added English text in the company page

---
+++ company/text en.php
@@ -0,0 +1,10 @@
+<?php
+const TEXT = [
+0 => '¿Por qué %s?',
+1 => 'Contacta con %s',
+2 => '%s en Instagram',
+3 => '¿Dónde está %s?',
+4 => 'Volver a la página principal',
+5 => 'Volver arriba',
+6 => 'Ver en Instagram',
+];
--- company/text es.php
+++ company/text es.php
@@ -1,10 +1,10 @@
 <?php
 const TEXT = [
-0 => '¿Por qué %s?',
-1 => 'Contacta con %s',
-2 => '%s en Instagram',
-3 => 'Mapa',
-4 => 'Volver a la pagina principal',
-5 => 'Volver arriba',
-6 => 'Ver en Instagram',
+0 => 'Why %s?',
+1 => 'Contact %s',
+2 => '%s on Instagram ',
+3 => 'Where is %s?',
+4 => 'Back to homepage',
+5 => 'Back to top',
+6 => 'View on Instagram',
 ];
