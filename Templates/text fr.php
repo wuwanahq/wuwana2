0daee3faf716773c32354eedbeeb928c8505a262
levogirar <yonathanchu@gmail.com>
2020-12-15 16:18:33 -0500

    added french language

---
+++ Templates/text fr.php
@@ -0,0 +1,7 @@
+<?php
+const TEMP_TEXT = [
+0 => "Vous cherchez torréfacteurs de café spécialité?",
+1 => "Trouvez rapidement les fournisseurs dont vous avez besoin.",
+2 => "Retour au début",
+3 => "Langues",
+];
---
+++ company/text fr.php
@@ -0,0 +1,12 @@
+<?php
+const TEXT = [
+0 => 'Pourquoi %s?',
+1 => 'Contactez %s',
+2 => '%s sur Instagram',
+3 => 'Localisation',
+4 => "Retour à la page d'accueil",
+5 => 'Visitez Instagram',
+6 => 'À propos de %s',
+7 => 'Adresse',
+8 => "Bonjour %s, je vous ai trouvé sur wuwana.com et j'aimerais en savoir plus sur vous",
+];
