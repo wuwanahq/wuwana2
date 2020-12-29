eda12782ce58b2c00b69acf0cc4e1759d998cf6a
levogirar <yonathanchu@gmail.com>
2020-12-28 22:33:11 -0500

    added statistics page

---
+++ admin/statistics/view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php echo $language ?>">
+<head>
+	<?php include '../../Templates/page metadata.php' ?>
+	<title>Admin page | Wuwana</title>
+	<link rel="stylesheet" type="text/css" href="/static/admin.css">
+	<script src="/static/admin.js" defer></script>
+</head>
+<body>
+	<?php include '../../Templates/page header.php' ?>
+	<div class="container">
+		<?php include '../../Templates/admin menu.php' ?>
+		<div class="column-main">
+			<section>
+				<h2>Statistics</h2>
+			</section>
+		</div>
+	</div>
+	<?php include '../../Templates/page footer.php' ?>
+</body>
+</html>
