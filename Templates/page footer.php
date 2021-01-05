f75b4a3a55643fc8e126490e6f733dd2ad56ef5b
levogirar <yonathanchu@gmail.com>
2021-01-05 16:38:39 -0500

    created footer

--- Templates/page footer.php
+++ Templates/page footer.php
@@ -1,3 +1,17 @@
 <a href="#">
 	<div id="toTop" class="button-icon back-top"><img src="/static/icon/arrow-circle-top.svg"><?php echo TEMP_TEXT[2] ?></div>
 </a>
+<footer>
+	<hr>
+	<ul>
+		<li>
+			<a href="/wuwana">About Wuwana™</a>
+		</li>
+		<li>
+			<a href="/">Our privacy policy</a> 
+		</li>
+		<li>
+			<a href="https://github.com/wuwanahq/wuwana2">We ❤️ Open Source</a>
+		</li>
+	</ul>
+</footer>
