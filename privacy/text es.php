b0ab46f689000f9c082bc7c843b68b8af1155841
levogirar <yonathanchu@gmail.com>
2021-01-06 13:17:37 -0500

    made footer dynamic

---
+++ privacy/text es.php
@@ -0,0 +1,37 @@
+<?php
+const TEXT = [
+// Text for Introduction section
+0 => 'Nuestra política de privacidad',
+1 => 'Versión: ',
+2 => 'Hola,',
+3 => 'En Wuwana defendemos la privacidad digital y hacemos todo lo posible para respetar su privacidad. Por eso sólo recogemos y utilizamos un mínimo de datos de nuestros usuarios.',
+4 => 'Aquí, encontrarás detalles completos sobre cómo Wuwana utiliza los datos y lo que recogemos sobre ti en esta página.',
+
+// Text for privacy-unique section
+5 => 'Sus datos explicados como visitante',
+6 => 'Como visitante, Wuwana no recopila ningún dato personal identificable, incluyendo nuestros análisis de web, donde sólo almacenamos datos no personales. Tampoco utilizamos cookies en nuestro sitio web, ni cookies de primera o tercera parte.',
+7 => 'Hacemos un seguimiento de las páginas que usted visita o de los enlaces en los que hace clic, pero no los almacenamos. No hay forma de que sepamos qué has visitado. Sólo sabemos que alguien lo hizo. Utilizamos esta información para ayudarnos a implementar funciones como el motor de búsqueda.',
+8 => 'Así que, como visitante, no almacenamos ningún dato personal sobre usted.',
+
+// Text for privacy-exception section
+9 => 'A tener en cuenta',
+10 => '1. Fundamentos técnicos',
+11 => "Nuestro servidor mantiene un registro de información técnica esencial, como su dirección IP, información del navegador o el sistema operativo. Su dispositivo envía automáticamente esta información, y sólo la utilizamos para las funciones principales de la web.",
+12 => '2. Enlaces y contenido de terceros',
+13 => "Mostramos los enlaces y el contenido de los sitios de terceros. Estos tienen sus políticas de privacidad independientes, y no tenemos ninguna responsabilidad por su contenido o actividades.",
+14 => "3. Uso de la información",
+15 => "Podemos revelar su información en respuesta a órdenes judiciales u otros requisitos legales; para ejercer nuestros derechos legales o defendernos contra reclamos legales; para investigar, prevenir o tomar medidas con respecto a actividades ilegales, sospecha de fraude o abuso, violaciones de nuestras políticas; o para proteger nuestros derechos y propiedad.",
+16 => "Podemos eliminar cualquier información en cualquier momento y por cualquier razón, como necesidades técnicas, preocupaciones legales, prevención de abusos, pérdida de datos o cualquier otra razón.",
+
+// Text for privacy-general section
+17 => "Su consentimiento",
+18 => "Al usar nuestro sitio, usted acepta nuestra política de privacidad.",
+19 => "Contactar con nosotros",
+20 => "Si tiene preguntas sobre esta política de privacidad, por favor envíenos un correo electrónico a jonathan@wuwana.com.",
+21 => "Cambios en esta política",
+22 => "Si decidimos cambiar nuestra política de privacidad, publicaremos esos cambios en esta página.",
+
+// Text for change log
+23 => "7 de enero de 2021 - Primera publicación"
+// Remember to update the HTML number code (below H1)
+];
---
+++ privacy/text fr.php
@@ -0,0 +1,37 @@
+<?php
+const TEXT = [
+// Text for Introduction section
+0 => "Notre politique de confidentialité",
+1 => "Version : ",
+2 => "Salut,",
+3 => "Chez Wuwana, nous nous soucions de la confidentialité et nous faisons de notre mieux pour défendre votre vie privée. C'est pourquoi nous collectons et utilisons uniquement des données minimales de nos utilisateurs.",
+4 => "Vous trouverez ici des détails complets sur la façon dont Wuwana utilise les données et sur ce que nous recueillons à votre sujet sur cette page.",
+
+// Text for privacy-unique section
+5 => "Vos données expliquées en tant que visiteur",
+6 => "En tant que visiteur, Wuwana ne collecte aucune donnée personnellement identifiable à votre sujet, y compris nos analyses, où nous ne stockons que des données non personnelles. Nous n'utilisons même pas de cookies sur notre site Web, ni de cookies propriétaires ni de cookies tiers.",
+7 => "Nous suivons les pages de l'entreprise que vous visitez ou les liens sur lesquels vous cliquez, mais nous ne les stockons pas. Nous n'avons aucun moyen de savoir que vous avez visité une page d'entreprise. Nous savons seulement que quelqu'un l'a fait. Nous utilisons ces informations pour nous aider à mettre en place des fonctionnalités comme les statistiques ou la recherche.",
+8 => "Ainsi, en tant que visiteur, nous ne stockons aucune de vos données personnelles.",
+
+// Text for privacy-exception section
+9 => "À prendre en compte",
+10 => "1. Bases techniques",
+11 => "Notre serveur tient un journal des informations techniques essentielles, telles que votre adresse IP, les informations relatives à votre navigateur ou votre système d'exploitation. Votre appareil envoie automatiquement ces informations, et nous ne les utilisons que pour les fonctionnalités de base du site web.",
+12 => "2. Liens et contenus tiers",
+13 => "Nous affichons des liens et des contenus de sites tiers. Ceux-ci ont leur propre politique de confidentialité, et nous n'avons aucune responsabilité quant à leur contenu ou leurs activités.",
+14 => "3. Utilisation des informations",
+15 => "Nous pouvons divulguer vos informations en réponse à des ordonnances du tribunal ou à d'autres exigences légales ; pour exercer nos droits légaux ou nous défendre contre des réclamations légales ; pour enquêter, prévenir ou prendre des mesures concernant des activités illégales, des suspicions de fraude ou d'abus, des violations de nos politiques ; ou pour protéger nos droits et notre propriété.",
+16 => "Nous pouvons supprimer toute information à tout moment et pour toute raison, comme les besoins techniques, les préoccupations juridiques, la prévention des abus, la perte de données ou toute autre raison.",
+
+// Text for privacy-general section
+17 => "Votre consentement",
+18 => "En utilisant notre site, vous acceptez notre politique de confidentialité.",
+19 => "Pour nous contacter",
+20 => "Si vous avez des questions concernant cette politique de confidentialité, veuillez nous envoyer un courriel à jonathan@wuwana.com.",
+21 => "Modifications de la présente politique",
+22 => "Si nous décidons de modifier notre politique de confidentialité, nous publierons ces changements sur cette page.",
+
+// Text for change log
+23 => "7 janvier 2021 - Première publication"
+// Remember to update the HTML number code (below H1)
+];
