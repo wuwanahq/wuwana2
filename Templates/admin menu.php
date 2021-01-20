ba7a971a284dea5e1d1083941d2729af84666246
levogirar <yonathanchu@gmail.com>
2021-01-20 11:34:50 -0500

    made admin-panel-box sticky

--- Templates/admin menu.php
+++ Templates/admin menu.php
@@ -9,29 +9,31 @@
 	];
 ?>
 <div class="column-left">
-	<div class="box-panel">
-		<div class="panel-cover"><img src="/static/logo/ribbon-long.svg" alt="wuwana ribbon logo"></div>
-		<section>
-			<h1>Administrator's name</h1>
-			<a href="#">
-				<div class="icon-label-h">
-					<img src="/static/icon/gray/close.svg" alt="">
-					Logout
-				</div>
-			</a>
-		</section>
-		<hr>
-		<section class="admin-control">
-			<h3>Admin controls</h3>
-			<?php foreach ($sections as $sectionURL => $section): ?>
-				<?php if ($url != $sectionURL) { echo '<a href="', $sectionURL, '">'; } ?>
-				<div class="icon-label-h">
-					<img src="/static/icon/gray/<?php echo $section[1] ?>" alt="">
-					<?php echo $section[0] ?>
-				</div>
-				<?php if ($url != $sectionURL) { echo '</a>'; } ?>
-			<?php endforeach ?>
-		</section>
+	<div class="sticky">
+		<div class="box-panel">
+			<div class="panel-cover"><img src="/static/logo/ribbon-long.svg" alt="wuwana ribbon logo"></div>
+			<section>
+				<h1>Administrator's name</h1>
+				<a href="#">
+					<div class="icon-label-h">
+						<img src="/static/icon/gray/close.svg" alt="">
+						Logout
+					</div>
+				</a>
+			</section>
+			<hr>
+			<section class="admin-control">
+				<h3>Admin controls</h3>
+				<?php foreach ($sections as $sectionURL => $section): ?>
+					<?php if ($url != $sectionURL) { echo '<a href="', $sectionURL, '">'; } ?>
+					<div class="icon-label-h">
+						<img src="/static/icon/gray/<?php echo $section[1] ?>" alt="">
+						<?php echo $section[0] ?>
+					</div>
+					<?php if ($url != $sectionURL) { echo '</a>'; } ?>
+				<?php endforeach ?>
+			</section>
+		</div>
+		<div id="version">Wuwana v2.1.10</div>
 	</div>
-	<div id="version">Wuwana v2.1.10</div>
 </div>
