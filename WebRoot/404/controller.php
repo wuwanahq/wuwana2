1deaad2ec47756dbf0201a4805d589cddda9054c
Vince <vincent.boursier@gmail.com>
2021-03-28 17:01:08 +0200

    try to find other companies on the 404 page

--- WebRoot/404/controller.php
+++ WebRoot/404/controller.php
@@ -6,8 +6,19 @@
 
 http_response_code(404);
 
-if (php_sapi_name() != 'cli-server')
-{ trigger_error('URL ' . filter_input(INPUT_SERVER, 'REQUEST_URI') . ' not found', E_USER_NOTICE); }
+$companies = [];
+$search = str_replace('/', '', $url);
+
+while (strlen($search) > 2 && count($companies) == 0)
+{
+	foreach ((new DataAccess\Company())->searchColumn('Company.Name', $search) as $permalink => $company)
+	{ $companies[$permalink] = $company; }
+
+	$search = substr($search, 0, -1);
+}
 
 require '404/text ' . $language->code . '.php';
 require '404/view.php';
+
+if (php_sapi_name() != 'cli-server')
+{ trigger_error('URL ' . filter_input(INPUT_SERVER, 'REQUEST_URI') . ' not found', E_USER_NOTICE); }
