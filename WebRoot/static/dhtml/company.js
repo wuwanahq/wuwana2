aabe6568424c98afb23db5b0f67e957aff6e1fcd
levogirar <yonathanchu@gmail.com>
2021-03-29 22:16:04 -0400

    worked on company summary intersection JS

--- WebRoot/static/dhtml/company.js
+++ WebRoot/static/dhtml/company.js
@@ -3,18 +3,14 @@
  * https://usefulangle.com/post/118/javascript-intersection-observer
  */
 
-var panel = document.getElementById("company-panel");
-var summary = document.getElementById("mobile-summary");
+const panel = document.getElementById("company-panel");
+const edit = document.getElementById("edit");
+const summary = document.getElementById("mobile-summary");
 
 // root is the browser viewport / screen
 var observer = new IntersectionObserver(function(entries) {
-	// since there is a single target to be observed, there will be only one entry
 	if(entries[0]['isIntersecting'] === true) {
-		if(entries[0]['intersectionRatio'] > 0.01){
-			summary.style.opacity = "0";
-		}
-		else if(entries[0]['intersectionRatio'] < 0.3)
-			summary.style.opacity = "1";
+		summary.style.opacity = "0";
 	}
 	else {
 		summary.style.opacity = "1";
@@ -22,20 +18,15 @@ var observer = new IntersectionObserver(function(entries) {
 }, { threshold: [0, 0.01, 1] });
 
 observer.observe(panel);
+// observer.observe(edit);
 
 // Function for the edit panel
-
 function companyEdit() {
-	const edit = document.getElementById("edit");
-	const panel = document.getElementById("company-panel");
-
-	console.log('hello');
-
 	if (panel.style.display == "flex") {
 		panel.style.display = "none";
-		edit.style.display = "flex"
+		edit.style.display = "flex";
 	} else {
 		panel.style.display = "flex";
-		edit.style.display = "none"
+		edit.style.display = "none";
 	}
 }
