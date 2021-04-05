9ea9369060ea1de9d15fa1e2d28090c86b7d1c9e
levogirar <jonathan.chulo@hey.com>
2021-04-04 22:57:36 -0400

    improved company css

--- WebRoot/company/view.php
+++ WebRoot/company/view.php
@@ -28,7 +28,7 @@
 	<div class="container">
 		<section class="column-left">
 			<div class="company-sticky">
-				<div class="box-panel" id="company-panel" style="display: flex">
+				<div id="company-panel" style="display: flex;">
 					<section class="company-about">
 						<img class="logo-main"
 							src="<?php echo $company->logo ?>"
